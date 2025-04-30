d854b70fe61dbbfedbe91c39a393525093ccc1ce
noelwatters757@gmail.com <youremail@example.com>
2025-04-30 12:51:17 -0400

    Updated Package.json

--- e-commerce/src/firebase/firebaseConfig.ts
+++ e-commerce/src/firebase/firebaseConfig.ts
@@ -7,6 +7,8 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // Firebase configuration using environment variables
+
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
